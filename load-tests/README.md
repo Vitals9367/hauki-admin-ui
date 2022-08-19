70078ee322c37cbb5494ace1b2c624ec41966dde
Pirkka Huhtala <pirkka.huhtala@gmail.com>
2022-08-19 11:54:21 +0300

    Add html reporter (#249)

--- load-tests/README.md
+++ load-tests/README.md
@@ -19,3 +19,11 @@ This script works as a wrapper for the k6 command and ensures that the proper au
 ```bash
 ./run.sh hauki-scenarios.js
 ```
+
+## HTML report
+The script is using [k6-reporter](https://github.com/benc-uk/k6-reporter) for outputting html test report. The results `summary.html` is created under this folder.
+
+You can also open it automatically after the test like this.
+```bash
+./run.sh hauki-scenarios.js && open summary.html
+```
--- load-tests/hauki-scenarios.js
+++ load-tests/hauki-scenarios.js
@@ -4,6 +4,8 @@ import { Httpx } from 'https://jslib.k6.io/httpx/0.0.1/index.js';
 import { createOpeningHour } from './helpers/mocks.js';
 import commands from './helpers/commands.js';
 import { formatDate, getRandomArbitrary } from './helpers/utils.js';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 const {
   AUTH_PARAMS: authParams,
@@ -98,4 +100,11 @@ export function teardown({ resourceId }) {
   });
 
   console.log(`Deleted date periods: ${toBeDeleted.length}`);
+}
+
+export function handleSummary(data) {
+  return {
+    'summary.html': htmlReport(data),
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
+  };
 }
