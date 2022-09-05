c0492378eaf5365e0aebbe0f22b5a9a6d84d20bb
Pirkka Huhtala <pirkka.huhtala@gmail.com>
2022-09-05 15:24:40 +0300

    Add helper text for opening hours titles (#264)

--- src/components/opening-hours-form/OpeningHoursTitles.tsx
+++ src/components/opening-hours-form/OpeningHoursTitles.tsx
@@ -13,29 +13,36 @@ const OpeningHoursTitles = ({ placeholders }: Props): JSX.Element => {
 
   return (
     <div className="card opening-hours-titles">
-      <TextInput
-        {...register('name.fi')}
-        data-test="opening-period-title-fi"
-        id="title-fi"
-        dat-test="opening-period-title-fi"
-        label="Aukioloajan otsikko suomeksi"
-        placeholder={placeholders.fi ?? ''}
-      />
-      <TextInput
-        {...register('name.sv')}
-        data-test="opening-period-title-sv"
-        id="title-sv"
-        label="Aukioloajan otsikko ruotsiksi"
-        placeholder={placeholders.sv ?? ''}
-      />
-      <TextInput
-        {...register('name.en')}
-        data-test="opening-period-title-en"
-        id="title-en"
-        name="name.en"
-        label="Aukioloajan otsikko englanniksi"
-        placeholder={placeholders.en ?? ''}
-      />
+      <div className="opening-hours-titles-inputs">
+        <TextInput
+          {...register('name.fi')}
+          aria-describedby="title-fi-helper-text"
+          data-test="opening-period-title-fi"
+          id="title-fi"
+          label="Aukioloajan otsikko suomeksi"
+          placeholder={placeholders.fi ?? ''}
+        />
+        <TextInput
+          {...register('name.sv')}
+          data-test="opening-period-title-sv"
+          id="title-sv"
+          label="Aukioloajan otsikko ruotsiksi"
+          placeholder={placeholders.sv ?? ''}
+        />
+        <TextInput
+          {...register('name.en')}
+          data-test="opening-period-title-en"
+          id="title-en"
+          name="name.en"
+          label="Aukioloajan otsikko englanniksi"
+          placeholder={placeholders.en ?? ''}
+        />
+      </div>
+      <p className="titles-helper-text" id="title-fi-helper-text">
+        Otsikko ei ole pakollinen. Tähän kohtaan voit kirjoittaa esim talviaika,
+        kevätkausi ym. Älä kirjoita aukiolokohdetta esim sauna, uima-allas,
+        kerros tms.
+      </p>
     </div>
   );
 };
